1bbfe6e004260f9627a9fcb1d7989b63883f9be5
Jacob Titus <pf5259yu@go.minnstate.edu>
2024-03-06 15:00:20 -0600

    Added the definition/implementation for the User and Game_Score data models to models.py

--- GeoGuessr/models.py
+++ GeoGuessr/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+# User/Player Class that stores the player's ID, username, password, and email
+class User:
+    def __init__(self, playerID, username, password, email):
+        self.playerID = playerID
+        self.username = username
+        self.password = password
+        self.email = email
+
+# Game_Scores Class that stores each game's ID, time to complete the game (finalTime), whether the
+# hints were used while playing the game (usedHintOne, usedHintTwo), what date the game was played
+# and will have the player's ID as a Foreign Key in the Database
+class Game_Score:
+    def __init__(self, gameID, score, finalTime, usedHintOne, usedHintTwo, datePlayed, playerID):
+        self.gameID = gameID
+        self.score = score
+        self.finalTime = finalTime
+        self.usedHintOne = usedHintOne
+        self.usedHintTwo = usedHintTwo
+        self.datePlayed = datePlayed
+        self.playerID = playerID
