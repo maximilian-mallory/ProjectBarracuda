0516cd5cf8c259ee6f05bfdc7101f8fb665e3570
Jacob Titus <pf5259yu@go.minnstate.edu>
2024-03-31 16:10:21 -0500

    Added the worldborder table to the PostgreSQL GeoGuessr database, and got load.run() to import and load GIS information into the worldborder relation.

--- Barracuda/settings.py
+++ Barracuda/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'Barracuda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'GeoGuessr',
         'USER': 'postgres',
         'PASSWORD': 'PB@rracuda1',
