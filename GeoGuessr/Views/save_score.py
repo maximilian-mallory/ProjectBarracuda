24b0b5c1a81b3ef207413588f12eb99f5c43ce6f
Maximilian <mallory.max99@gmail.com>
2024-04-07 16:37:47 -0500

    Got rid of errors surrounding post request. Now I still have to create data to POSTGRES but I'm at least getting the data to the python Views

--- GeoGuessr/Views/save_score.py
+++ GeoGuessr/Views/save_score.py
@@ -2,35 +2,26 @@ from GeoGuessr.Models.game_score import Game_Score
 import datetime, random
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
+from django.utils import timezone
 
 @csrf_exempt
-def SaveScore( request ):
+def SaveScore(request):
     if request.method == 'POST':
-
-        data = request.POST
-        print(data)
-        time_left = data.get('timeLeft')
-        print(time_left)
-
-        num_guesses = data.get('numGuesses')
-        print(num_guesses)
-        player_id = 1
-        score = 1000 - abs(int(time_left) - 35) - (50 * int(num_guesses) )
-        
-        current_time_utc = datetime.datetime.now()
-        
         try:
-            game_score = Game_Score.objects.create(
-                score  = score,
-                finalTime = time_left,
-                usedHintOne = False,
-                usedHintTwo = False,
-                datePlayed = current_time_utc,
-                playerID = player_id
-            )
-            print("success!")
+            data = json.loads(request.body)
+            time_left = data.get('timeLeft')
+            num_guesses = data.get('numGuesses')
+
+            if time_left is None or num_guesses is None:
+                raise ValueError("Invalid data received")
+
+            player_id = 1
+            score = 1000 - abs(int(time_left) - 35) - (50 * int(num_guesses))
+            current_time_utc = datetime.datetime.now()
+            current_time_utc = timezone.now()
+
+            print(score, current_time_utc, player_id)
             return JsonResponse({'message': 'Game score saved successfully'})
         except Exception as e:
-            print("fail")
             return JsonResponse({'error': str(e)}, status=500)
-
