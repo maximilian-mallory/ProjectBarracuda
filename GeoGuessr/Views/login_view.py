0e923bb4dc361e82a876ecca258fc3cef553ce69
Maximilian <mallory.max99@gmail.com>
2024-04-07 19:44:40 -0500

    Recommit I cleaned some stuff up

--- GeoGuessr/Views/login_view.py
+++ GeoGuessr/Views/login_view.py
@@ -16,28 +16,26 @@ def Login(request):
 def LoginVerify(request):
     if request.method == 'POST':
         try:
-            # Get the username and password from the request
             username = request.POST.get('username')
             password = request.POST.get('password')
             print(username)
-            # Retrieve the user object from the database based on the username
+
             try:
                 user = User.objects.get(username=username)
             except User.DoesNotExist:
-                # User with the provided username doesn't exist
+
                 return JsonResponse({'message': 'User does not exist'})
 
-            # Hash the incoming password using SHA1
             hashed_password = hashlib.sha1(password.encode()).hexdigest()
             print(hashed_password)
-            # Compare the hashed password with the hashed password stored in the database
+
             if user.password == hashed_password:
-                # Passwords match, authentication successful
+
                 return JsonResponse({'message': 'Login successful'})
             else:
-                # Passwords don't match, authentication failed
+
                 return JsonResponse({'message': 'Incorrect password'})
 
         except Exception as e:
-            # Handle any other errors
+
             return JsonResponse({'error': str(e)}, status=500)
