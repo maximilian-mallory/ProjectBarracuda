6730fcba36aac6861486ccf844f7e8de2a31692e
Maximilian <mallory.max99@gmail.com>
2024-04-12 11:50:03 -0500

    Leaderboard view was called 'Signup' from copy and pase, urls.py was fine but got mad. Refactored.

--- GeoGuessr/Views/leaderboard_view.py
+++ GeoGuessr/Views/leaderboard_view.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 import os, random
 from django.conf import settings
 
-def Signup(request):
+def Leaderboard(request):
     context = {}
     return render(request, 'LeaderboardPage.html')   
