31d5cc2bfb13179e4860c3c147eaf25d000b7c06
Maximilian <mallory.max99@gmail.com>
2024-04-15 16:10:59 -0500

    Made the foreign key relationship between game_score and player

--- GeoGuessr/Models/game_score.py
+++ GeoGuessr/Models/game_score.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from GeoGuessr.Models.user import User
+
 # Game_Scores Class that stores each game's ID, time to complete the game (finalTime), whether the
 # hints were used while playing the game (usedHintOne, usedHintTwo), what date the game was played
 # and will have the player's ID as a Foreign Key in the Database
@@ -11,4 +13,4 @@ class Game_Score(models.Model):
     usedHintOne = models.BooleanField(default=False)
     usedHintTwo = models.BooleanField(default=False)
     datePlayed = models.DateTimeField()
-    playerID = models.IntegerField()
+    playerID = models.ForeignKey(User, on_delete=models.CASCADE)
