a1d056fa921db27b2f45f2cab937aa212ebba80a
Maximilian <mallory.max99@gmail.com>
2024-04-30 16:39:23 -0500

    Logic applied for use of hint two, state bird

---
+++ GeoGuessr/Views/bird_watching_view.py
@@ -0,0 +1,22 @@
+from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+from GeoGuessr.Controllers.bird_controller import SetBird
+
+@csrf_exempt
+def GetBird(request):
+
+    data = json.loads(request.body)
+    city = data.get('city')
+    print(city)
+    bird = SetBird(city)
+    print(bird);
+    try:
+
+        bird = SetBird(city)
+        print(bird);
+        return JsonResponse({'bird': bird})
+    
+    except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
