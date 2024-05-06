bbe520aba84115a650dc95712cb6b9b9f8a02c2a
Maximilian <mallory.max99@gmail.com>
2024-05-05 19:30:31 -0500

    cleaning up files and more refactoring

---
+++ GeoGuessr/Static/js/leaderboard.js
@@ -0,0 +1,50 @@
+toWelcome.onclick = function() {
+    window.location.href = "http://localhost:8000/welcome/"; // Change this to your desired URL
+};
+function formatTime(seconds) {
+    const date = new Date(seconds * 1000);
+    return date.toLocaleTimeString('en-US', { minute: '2-digit', second: '2-digit' });
+}
+
+function loadAllScores() {
+    const leaderboardBody = document.getElementById("leaderboardBody");
+    leaderboardBody.innerHTML = "";
+    let i = 0;
+    allScores.forEach(entry => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${++i}</td>
+            <td>${entry.fields.username}</td>
+            <td>${entry.fields.score}</td>
+            <td>${formatTime(Math.abs(entry.fields.finalTime - 300))}</td>
+        `;
+        leaderboardBody.appendChild(row);
+    });
+}
+
+function loadUserScores() {
+    const leaderboardBody = document.getElementById("leaderboardBody");
+    leaderboardBody.innerHTML = "";
+    let i = 0;
+    allScores.forEach(entry => {
+        if(entry.fields.username == username)
+        {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td>${++i}</td>
+                <td>${entry.fields.username}</td>
+                <td>${entry.fields.score}</td>
+                <td>${formatTime(Math.abs(entry.fields.finalTime - 300))}</td>
+            `;
+            leaderboardBody.appendChild(row);
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    allScoresButton.onclick = loadAllScores;
+    userScoresButton.onclick = loadUserScores;
+    window.onload = loadAllScores;
+
+});
---
+++ GeoGuessr/Static/js/signup.js
@@ -0,0 +1,37 @@
+toWelcome.onclick = function() {
+    window.location.href = "http://localhost:8000/welcome/"; // Change this to your desired URL
+};
+document.addEventListener('DOMContentLoaded', function() {
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username').value.trim();
+  const password = CryptoJS.SHA1(document.querySelector('#password').value.trim()).toString();
+
+  const data = {
+    username: username,
+    password: password
+  };
+  console.log(data);
+
+  fetch('/sign-up-verify/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+    })
+    .then(response => {
+      if( response.ok )
+      {
+        window.location.href = '/welcome/';
+      } 
+    }
+    )
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
+});
